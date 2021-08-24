d365934e111ea5612c7cf4bc2cb7819da7ff0a8c
coderblack <coderblack@163.com>
2021-08-24 15:42:51 +0800

    25期spark代码工程初始化

---
+++ src/main/scala/cn/doitedu/spark/demos/C01_入门程序WordCount.scala
@@ -0,0 +1,44 @@
+package cn.doitedu.spark.demos
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * @author 涛哥
+ * @nick_name "deep as the sea"
+ * @contact qq:657270652 wx:doit_edu
+ * @site www.doitedu.cn
+ * @date 2021-08-24
+ * @desc 深似海男人的一个深似海的程序
+ */
+object C01_入门程序WordCount {
+
+  def main(args: Array[String]): Unit = {
+    // 构造一个spark的参数配置对象
+    val conf = new SparkConf()
+    conf.setMaster("local")  // 以单线程运行
+    // conf.setMaster("local[*]")  // 以机器上的cpu线程数来作为并发数
+    // conf.setMaster("local[2]")  // 以指定的数字的线程数作为并发数
+    conf.setAppName("深似海男人的一个深似海程序")
+
+
+    // 构造一个spark的driver工具
+    val sc: SparkContext = new SparkContext(conf)
+
+
+    // 1.加载数据（读数据）
+    val rdd1: RDD[String] = sc.textFile("data/wordcount.txt")   // a a a a a b c
+
+    // 2.写计算逻辑
+    val rdd2: RDD[String] = rdd1.flatMap(s => s.split("\\s+"))  // a
+    val rdd3: RDD[(String, Iterable[String])] = rdd2.groupBy(x => x)
+    val rdd4: RDD[(String, Int)] = rdd3.mapValues(iter => iter.size)
+
+    // 3.输出结果
+    rdd4.foreach(println)
+
+
+    sc.stop()
+  }
+
+}
---
+++ src/main/scala/cn/doitedu/spark/demos/HelloWorld.scala
@@ -0,0 +1,10 @@
+package cn.doitedu.spark.demos
+
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+
+    println("hello doitedu")
+
+
+  }
+}
